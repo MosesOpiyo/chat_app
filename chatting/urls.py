52f84c55fd17a5afc6b41607aa7fa8d363a7778f
Moses <moses.junior@student.moringaschool.com>
2021-11-29 12:49:05 +0300

    adding serializer and views functionality

--- chatting/urls.py
+++ chatting/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/account/',include('users.api.urls')),
 ]

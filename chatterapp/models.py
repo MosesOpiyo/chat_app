12e0ece0519db6d7fa7f00c269752086f5911c39
Moses <moses.junior@student.moringaschool.com>
2021-11-29 11:32:57 +0300

    adding apps handling accounts and chatboxes

--- chatterapp/models.py
+++ chatterapp/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+class Chatbox(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    members = models.OneTo
+
+class Message(models.Model):
+    text = models.TextField()
+    texter = models.ForeignKey(User,on_delete=CASCADE)
+    time_input = models.TimeField()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,null=True)
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+    profile_image = models.ImageField(blank=True,null=True,)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,Group,PermissionsMixin
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+class MyAccountManager(BaseUserManager):
+    """defines the methods to manage the custom user to be created
+    Args:
+        BaseUserManager ([type]): [description]
+    Returns:
+        [type]: [description]
+    """
+    def create_user(self,email,username,password=None):
+        if not email:
+            raise ValueError("Users must have and email address")
+        if not username:
+            raise ValueError("Users must have a username")
+        
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            password = password
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,email,username,password):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+        )
+
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+        return user
+    
+class Account(PermissionsMixin,AbstractBaseUser)

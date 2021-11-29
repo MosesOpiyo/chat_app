f6a20078882eceda47eec2d192ce910a2fbd1ddf
Moses <moses.junior@student.moringaschool.com>
2021-11-29 12:17:30 +0300

    changes in models.py

--- chatterapp/models.py
+++ chatterapp/models.py
@@ -1,20 +1,39 @@
 from django.contrib.auth.models import User
+from django.core.checks import messages
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
 
+from users.models import Account
+
 class Chatbox(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=1000)
-    members = models.OneTo
-
-class Message(models.Model):
-    text = models.TextField()
-    texter = models.ForeignKey(User,on_delete=CASCADE)
-    time_input = models.TimeField()
-
-class Profile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE,null=True)
-    name = models.CharField(max_length=100)
+    """This defines the behaviours of a project
+    Args:
+        models ([type]): [description]
+    """
+    owner = models.ForeignKey(Account,on_delete=models.RESTRICT)
+    name = models.CharField(max_length=50,unique=True)
     bio = models.TextField()
-    profile_image = models.ImageField(blank=True,null=True,)
+    date_created = models.DateField()
+    chatbox_pic = models.ImageField()
+    
+    def __str__(self):
+        return self.name
+    
+    def update(self,new):
+        self.name = new.name
+        self.bio = new.bio
+        self.chatbox_pic = new.chatbox_pic
+        self.save()
+
+class Chat(models.Model):
+    admin = models.ForeignKey(Account,on_delete=models.RESTRICT)
+    chatbox = models.ForeignKey(Chatbox,on_delete=CASCADE)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.admin.username + " comment | " + str(self.chatbox.name)
+
+
+   
+    
--- users/models.py
+++ users/models.py
@@ -38,4 +38,42 @@ class MyAccountManager(BaseUserManager):
         user.save(using=self._db)
         return user
     
-class Account(PermissionsMixin,AbstractBaseUser)
+class Account(PermissionsMixin,AbstractBaseUser):
+    """This will define the custom user model to be used
+    Args:
+        AbstractBaseUser ([type]): [description]
+    """
+    email = models.EmailField(verbose_name="email",max_length=100,unique=True)
+    username = models.CharField(max_length=100,unique=True)
+    date_joined = models.DateTimeField(verbose_name="date joined",auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name="last login",auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    profile_pic = models.ImageField(null=True,blank=True)
+
+    objects = MyAccountManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self,perm,obj=None):
+        return self.is_admin
+
+    def has_module_perms(self,app_label):
+        return True
+
+    def delete_user(self):
+        self.delete()
+
+    def inactivate(self):
+        self.is_active = False
+        self.save()
+
+
+    
